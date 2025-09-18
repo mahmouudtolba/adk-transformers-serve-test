115e9c7097a547b08ea2fbfc20fe3069fb1d72e2
mahmoud tolba <mahmoudtolba@mahmouds-MacBook-Air.local>
2025-09-18 15:42:59 +0300

    test2

--- 1-basic-agent/tool_agent/agent.py
+++ 1-basic-agent/tool_agent/agent.py
@@ -7,15 +7,15 @@ import litellm
 from google.adk.agents import Agent
 from google.adk.models.lite_llm import LiteLlm
 import random
-
+litellm._turn_on_debug()
 
 # --- Tool function ---
 def get_random_number() -> int:
     """Return a random integer between 1 and 100."""
     return random.randint(1, 100)
 
-
-local_llm = LiteLlm(model="ollama_chat/qwen3:4b")
+local_llm = LiteLlm(model="huggingface/Qwen/Qwen3-0.6B",
+api_base_url ="http://localhost:8000/v1" )
 
 root_agent = Agent(
     model=local_llm ,
